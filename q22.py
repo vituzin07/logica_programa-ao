dc4b071589c84b29fd1f65f42b040cf640a9ff4f
Joâo victor <jv2644099@gmail.com>
2023-11-01 16:24:48 -0300

    feitas

--- q22.py
+++ q22.py
@@ -1,11 +1,20 @@
-print(type(2.25-1))
-print(type(3.0*3))
-print(type(2*4))
-print(type(2.0**4))
-print(type(2/2.0))
-print(type(6/4))
-print(type(6%4))
-print(type(4%2))
+#item a
+print(2.25-1,type(2.25-1))
+#item b
+print(3.0*3,type(3.0*3))
+#item c
+print(2*4,type(2*4))
+#item d
+print(2.0**4,type(2.0**4))
+#item e
+print(2/2.0,type(2/2.0))
+#item f
+print(6/4,type(6/4))
+#item g
+print(6%4,type(6%4))
+#item h
+print(4%2,type(4%2))
+#item i
 numero = 0.58
 numero_arrendondado = round(numero, 1)
 print(numero_arrendondado)
