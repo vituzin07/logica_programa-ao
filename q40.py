33ec751441981ead4850d3e3a731f3cc03b98886
Joâo victor <jv2644099@gmail.com>
2023-11-13 14:34:21 -0300

    feitas

--- q40.py
+++ q40.py
@@ -3,7 +3,9 @@
 #item a
 Item_a = 3<2**3 and 3==3
 print(Item_a)
+
 #primeiro foi resolvido a operaçao dos itens entre parenteses, depois a divisao seguida da soma. operador de relaçao e por ultimo o operado logico
+
 #item b 
 Item_b = 0!=4 or (3/3==1 and (5+1)/3==2)
 print(Item_b)
