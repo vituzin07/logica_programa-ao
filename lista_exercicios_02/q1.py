a986abc90984c2cce541b3b809d75848cdb0cc8a
joao victor <jv2644099@gmail.com>
2023-11-29 08:53:33 -0300

    feitas

---
+++ lista_exercicios_02/q1.py
@@ -0,0 +1,6 @@
+numero_1=int(input('Insira o 1º número: '))
+numero_2=int(input('Insira o 2º número: '))
+numero_3=int(input('Insira o 3º número: '))
+match numero_1 and numero_2 and numero_3:
+    case
+        print (numero_1)
---
+++ q.41.py
@@ -0,0 +1,6 @@
+
+salario = float(input("Digite o valor do salário: "))
+
+deve_pagar_imposto = salario > 1200.00
+
+print("Paga imposto?", deve_pagar_imposto)
