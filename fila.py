19c0dfa9eafddde0f2777b1d51e02311a5573c03
Joâo victor <jv2644099@gmail.com>
2023-12-06 15:49:31 -0300

    feitas

---
+++ fila.py
@@ -0,0 +1,22 @@
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print('''F - Adicionar ao fim da fila
+A - Atende uma pessoa
+S - Sair do sistema\n''')
+while True:
+    escolha = str(input('''F - Adicionar ao fim da fila
+A - Atende uma pessoa
+S - Sair do sistema\n'''))
+    if escolha == 'F':
+        lista.insert(11, len(lista)+1)
+        print(lista)
+        print(len(lista))
+    elif escolha == 'A':
+        del lista[0]
+        print(lista)
+        print(len(lista))   
+    elif escolha == 'S':
+        print('Você saiu do sistema.')
+        break
+    escolha = str(input('''F - Adicionar ao fim da fila
+A - Atende uma pessoa
+S - Sair do sistema\n'''))
