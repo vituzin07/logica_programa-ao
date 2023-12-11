8c4614b63f0d648bf4fe9a2b2d0a362be6f1b2ea
Joâo victor <jv2644099@gmail.com>
2023-12-11 15:42:30 -0300

    feitas

---
+++ lista_exercicicos_03/q10.py
@@ -0,0 +1,15 @@
+#questão a
+def area_quadrado(lado):
+    return lado*lado
+
+
+soma_1 = area_quadrado(4)
+soma_2 = area_quadrado(9)
+print(soma_1, soma_2)
+
+#questão b
+def area_triangulo (base, altura):
+    return base*altura/2
+soma_3 = area_triangulo(6, 9)
+soma_4 = area_triangulo(5, 8)
+print(soma_3, soma_4)
