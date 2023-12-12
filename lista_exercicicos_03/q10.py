f892c28a2aaaef5dde9cf8030e3411b8bd324b48
Joâo victor <jv2644099@gmail.com>
2023-12-12 15:09:46 -0300

    feias

--- lista_exercicicos_03/q10.py
+++ lista_exercicicos_03/q10.py
@@ -1,15 +1,11 @@
 #questão a
 def area_quadrado(lado):
     return lado*lado
-
-
-soma_1 = area_quadrado(4)
-soma_2 = area_quadrado(9)
-print(soma_1, soma_2)
+x = area_quadrado(4), area_quadrado(9)
+print(x)
 
 #questão b
 def area_triangulo (base, altura):
     return base*altura/2
-soma_3 = area_triangulo(6, 9)
-soma_4 = area_triangulo(5, 8)
-print(soma_3, soma_4)
+z = area_triangulo(6, 9), area_triangulo(5, 8)
+print(z)
