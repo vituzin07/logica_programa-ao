c7bb33dcebee5649b6923a53056c0bdcc6805290
Joâo victor <jv2644099@gmail.com>
2023-12-13 13:49:36 -0300

    feitas

--- lista_exercicicos_03/q10.py
+++ lista_exercicicos_03/q10.py
@@ -1,11 +1,13 @@
 #questão a
-def area_quadrado(lado):
-    return lado*lado
-x = area_quadrado(4), area_quadrado(9)
+def area_quadrado(lado, exibir=False):
+    if exibir:
+        return lado*lado
+x = area_quadrado(4, True), area_quadrado(9, True)
 print(x)
 
 #questão b
-def area_triangulo (base, altura):
-    return base*altura/2
-z = area_triangulo(6, 9), area_triangulo(5, 8)
+def area_triangulo (base, altura, exibir=False):
+    if exibir:
+        return base*altura/2
+z = area_triangulo(6, 9, True ), area_triangulo(5, 8, True)
 print(z)
